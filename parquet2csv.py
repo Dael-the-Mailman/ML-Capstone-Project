908a7c09f8c5d91ba40b52bb37598de39dcce34c
Kaleb Ugalde <kalebugalde@gmail.com>
2022-07-30 20:55:23 -0700

    Google AutoML failed :(

--- parquet2csv.py
+++ parquet2csv.py
@@ -3,6 +3,6 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-FILENAME = "train_fe_plus_plus"
+FILENAME = "test_fe"
 df = pd.read_parquet(config["ENGINEERED_DATA"] + f"{FILENAME}.parquet")
 df.to_csv(config["ENGINEERED_DATA"] + f"{FILENAME}.csv")
