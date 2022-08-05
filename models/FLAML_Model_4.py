2cfa44a54e2625aa688c81a94ab34f1a8c712eb3
Kaleb Ugalde <kalebugalde@gmail.com>
2022-08-04 21:37:25 -0700

    Testing new extended training features

--- models/FLAML_Model_4.py
+++ models/FLAML_Model_4.py
@@ -35,43 +35,49 @@ if __name__ == "__main__":
     X_train = train[FEATURES]
     y_train = train["target"]
 
-    # Part 1A: Classification Model
-    print("Training Classification Model")
-    automl = AutoML()
-    automl.fit(X_train, y_train, task="classification", time_budget=3600)
-
-    print('Best Classification ML learner:', automl.best_estimator)
-    print('Best hyperparmeter config:', automl.best_config)
-    print('Best accuracy on validation data: {0:.4g}'.format(1-automl.best_loss))
-    print('Training duration of best run: {0:.4g} s'.format(automl.best_config_train_time))
-    
-    print("Dumping hyperparameter json")
-    with open(f"{automl.best_estimator}_classification_fe.json", "w") as fp:
-        model = {automl.best_estimator : automl.best_config}
-        json.dump(model, fp)
-
-    print("Saving Model")
-    class_path = config["FLAML_MODELS"] + f'automl_classification_fe_{(1-automl.best_loss):.4g}.pkl'
-    with open(class_path, 'wb') as f:
-        pickle.dump(automl, f, pickle.HIGHEST_PROTOCOL)
-
-    # Part 1B: Regression Model
+    # Part 1A: Regression Model
     print("Training Regression Model")
     automl = AutoML()
     automl.fit(X_train, y_train, task="regression", time_budget=3600)
 
     print('Best Regression ML learner:', automl.best_estimator)
     print('Best hyperparmeter config:', automl.best_config)
-    print('Best accuracy on validation data: {0:.4g}'.format(1-automl.best_loss))
+    print('Best loss on validation data: {0:.4g}'.format(automl.best_loss))
     print('Training duration of best run: {0:.4g} s'.format(automl.best_config_train_time))
+    
     print("Dumping hyperparameter json")
     with open(f"{automl.best_estimator}_regression_fe.json", "w") as fp:
         model = {automl.best_estimator : automl.best_config}
         json.dump(model, fp)
 
     print("Saving Model")
-    reg_path = config["FLAML_MODELS"] + f'automl_regression_fe_{(1-automl.best_loss):.4g}.pkl'
+    reg_path = config["FLAML_MODELS"] + f'automl_regression_fe_{(automl.best_loss):.4g}.pkl'
     with open(reg_path, 'wb') as f:
+        pickle.dump(automl, f, pickle.HIGHEST_PROTOCOL)
+
+    # Part 1B: Extended Regression Model
+    print("Loading Extended Train FE Data")
+    train = pd.read_parquet(config["ENGINEERED_DATA"] + "train_fe.parquet")
+    FEATURES = train.columns[1:-1]
+    X_train = train[FEATURES]
+    y_train = train["target"]
+
+    print("Training Extended Regression Model")
+    automl = AutoML()
+    automl.fit(X_train, y_train, task="regression", time_budget=3600)
+
+    print('Best Extended Regression ML learner:', automl.best_estimator)
+    print('Best hyperparmeter config:', automl.best_config)
+    print('Best loss on validation data: {0:.4g}'.format(automl.best_loss))
+    print('Training duration of best run: {0:.4g} s'.format(automl.best_config_train_time))
+    print("Dumping hyperparameter json")
+    with open(f"{automl.best_estimator}_extended_regression_fe.json", "w") as fp:
+        model = {automl.best_estimator : automl.best_config}
+        json.dump(model, fp)
+
+    print("Saving Model")
+    ext_reg_path = config["FLAML_MODELS"] + f'automl_extended_regression_fe_{(1-automl.best_loss):.4g}.pkl'
+    with open(ext_reg_path, 'wb') as f:
         pickle.dump(automl, f, pickle.HIGHEST_PROTOCOL)
 
     # Part 1C: Load and predict
@@ -79,81 +85,16 @@ if __name__ == "__main__":
     del train, X_train, y_train
     test = pd.read_parquet(config["ENGINEERED_DATA"] + "test_fe.parquet")
     test = test.drop("customer_ID", axis=1)
-    for path in [class_path, reg_path]:
+    for path in [ext_reg_path, reg_path]:
         with open(path, 'rb') as f:
             automl = pickle.load(f)
         sub = automl.predict(test)
         labels = pd.read_csv(config["SAMPLE_PATH"])
         labels["prediction"] = sub
-        if path == class_path:
-            print("Saving Classifcation Submission")
-            labels.to_csv(config["SUBMISSION_FOLDER"] + "flaml_classification_fe_submission.csv")
+        if path == ext_reg_path:
+            print("Saving Extended Regression Submission")
+            labels.to_csv(config["SUBMISSION_FOLDER"] + "flaml_extended_regression_fe_submission.csv")
         else:
             print("Saving Regression Submission")
             labels.to_csv(config["SUBMISSION_FOLDER"] + "flaml_regression_fe_submission.csv")
-    del labels, sub
-
-    import sys
-    sys.exit()
-
-    # Part 2: Using train_fe_plus_plus and test_fe_plus_plus
-    print("Loading Train FE Plus Plus Data")
-    train = pd.read_parquet(config["ENGINEERED_DATA"] + "train_fe_plus_plus.parquet")
-    FEATURES = train.columns[1:-1]
-    X_train = train[FEATURES]
-    y_train = train["target"]
-
-    # Part 2A: Classification Model
-    print("Training Classification Model")
-    automl = AutoML()
-    automl.fit(X_train, y_train, task="classification", ensemble=True, time_budget=600)
-
-    print('Best Classification ML learner:', automl.best_estimator)
-    print('Best hyperparmeter config:', automl.best_config)
-    print('Best accuracy on validation data: {0:.4g}'.format(1-automl.best_loss))
-    print('Training duration of best run: {0:.4g} s'.format(automl.best_config_train_time))
-    print("Dumping hyperparameter json")
-    with open(f"{automl.best_estimator}_classification_fe_plus.json", "w") as fp:
-        model = {automl.best_estimator : automl.best_config}
-        json.dump(model, fp)
-
-    print("Saving Model")
-    class_path = config["FLAML_MODELS"] + f'automl_classification_fe_plus_{(1-automl.best_loss):.4g}.pkl'
-    with open(class_path, 'wb') as f:
-        pickle.dump(automl, f, pickle.HIGHEST_PROTOCOL)
-
-    # Part 2B: Regression Model
-    print("Training Regression Model")
-    automl = AutoML()
-    automl.fit(X_train, y_train, task="regression", ensemble=True, time_budget=600)
-
-    print('Best Regression ML learner:', automl.best_estimator)
-    print('Best hyperparmeter config:', automl.best_config)
-    print('Best accuracy on validation data: {0:.4g}'.format(1-automl.best_loss))
-    print('Training duration of best run: {0:.4g} s'.format(automl.best_config_train_time))
-    print("Dumping hyperparameter json")
-    with open(f"{automl.best_estimator}_regression_fe_plus.json", "w") as fp:
-        model = {automl.best_estimator : automl.best_config}
-        json.dump(model, fp)
-
-    print("Saving Model")
-    reg_path = config["FLAML_MODELS"] + f'automl_regression_fe_plus_{(1-automl.best_loss):.4g}.pkl'
-    with open(reg_path, 'wb') as f:
-        pickle.dump(automl, f, pickle.HIGHEST_PROTOCOL)
-
-    # Part 2C: Load and predict
-    print("Load and predict test data")
-    del train, X_train, y_train
-    test = pd.read_parquet(config["ENGINEERED_DATA"] + "test_fe_plus_plus.parquet")
-    test = test.drop("customer_ID", axis=1)
-    for path in [class_path, reg_path]:
-        with open(path, 'rb') as f:
-            automl = pickle.load(f)
-        sub = automl.predict(test)
-        labels = pd.read_csv(config["SAMPLE_PATH"])
-        labels["prediction"] = sub
-        if path == class_path:
-            labels.to_csv(config["SUBMISSION_FOLDER"] + "flaml_classification_fe_plus_submission.csv")
-        else:
-            labels.to_csv(config["SUBMISSION_FOLDER"] + "flaml_regression_fe_plus_submission.csv")
     del labels, sub
