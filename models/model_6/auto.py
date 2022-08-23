9505ba0f70f913b1496888993eae68c67c9d7870
Kaleb Ugalde <kalebugalde@gmail.com>
2022-08-23 00:33:03 -0700

    Created Reduced Dimension dataset

--- models/model_6/auto.py
+++ models/model_6/auto.py
@@ -11,9 +11,12 @@ if __name__ == '__main__':
     train = pd.read_parquet(config["ENGINEERED_DATA"] + "train_fe.parquet")
     test = pd.read_parquet(config["ENGINEERED_DATA"] + "test_fe.parquet")
 
+    train.drop("customer_ID", axis=1, inplace=True)
+    test.drop("customer_ID", axis=1, inplace=True)
+
     target = 'target'
     sample_submission = pd.read_csv(config["SAMPLE_PATH"])
-    scoring_parameter = 'roc_auc'
+    scoring_parameter = 'logloss'
     VERSION = 1
 
     model, features, trainm, testm = Auto_ViML(
@@ -33,7 +36,7 @@ if __name__ == '__main__':
         verbose=0,
     )
 
-    model_path = config["FLAML_MODELS"] + f'viml_V{VERSION}.pkl'
+    model_path = config["VIML_MODELS"] + f'viml_V{VERSION}.pkl'
     with open(model_path, 'wb') as f:
         pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
     
